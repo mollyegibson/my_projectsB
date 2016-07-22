068fa88a02372998de5d04cefa704996fcd8ca2f
Molly Gibson <mollyegibson@gmail.com>
2016-07-22 15:07:59 -0500

    Calendar is working

--- getevents.php
+++ getevents.php
@@ -31,12 +31,13 @@ echo json_encode($events);
 
 $stmt->close();
 
-?>
-
-<script type="text/javascript">
+/*<script type="text/javascript">
 
 var eventData = <?php echo json_encode($events, JSON_PRETTY_PRINT) ?>;
 
-console.log(eventData[0].event_name);
+alert(eventData[0].event_name);
 
-</script>
+</script>*/ // only within the PHP bc it wont let me comment things out elsewhere
+
+?>
+
